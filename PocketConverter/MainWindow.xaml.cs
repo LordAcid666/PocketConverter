2216839c957fe0d1edbc0953aab253aca393ef88
Casalnovo Giacomo <gc76@live.it>
2023-02-22 19:41:32 +0100

    title changed

--- PocketConverter/MainWindow.xaml.cs
+++ PocketConverter/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PocketConverter
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
